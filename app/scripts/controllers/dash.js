6276a3f1ad2946deaddf841a95817c6897002457
Daniel Choudhury <dannychoudhury@gmail.com>
2013-11-21 12:57:54 +0000

    Working around javascript closure. Woops!

--- app/scripts/controllers/dash.js
+++ app/scripts/controllers/dash.js
@@ -4,9 +4,15 @@ angular.module('energenieApp')
   .controller('DashCtrl', function ($scope, User) {
 
   	$scope.name = User.User;
-  	$scope.gas = User.gas;
-  	$scope.water = User.water;
-  	$scope.elec = User.elec;
+  	$scope.bills = [];
 
+  	$scope.addBill = function(bill){
+  		var newBill ={};
+  		newBill.type = bill.type;
+  		newBill.amount = bill.amount;
+  		newBill.date = bill.date;
+
+  		$scope.bills.push(newBill);
+  	}
 
   });
